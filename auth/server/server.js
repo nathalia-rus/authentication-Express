67c9e64f503f152f177ecb759a337fb0eb39bf5a
Nathalia Rus <nathaliarus@live.fr>
2018-09-18 12:10:01 +0200

    uuid session basics

--- auth/server/server.js
+++ auth/server/server.js
@@ -1,11 +1,28 @@
 // npm modules
 const express = require('express');
+const uuid = require('uuid/v4');
+const session = require('express-session')
+
 // create the server 
 const app = express();
 
+// add and configure middleware 
+app.use(session({
+  genid: (req) => {
+    console.log('inside the session middleware')
+    console.log(req.sessionID)
+    return uuid()  // use UUIDs for session IDs
+  },
+  secret : 'keyboard cat', // see comment below
+  resave: false,
+  saveUninitialized: true
+}))
+
 //  creates the homepage route at '/'
 app.get('/', (req, res) => {
-  res.send('you just hit the home page\n')
+  console.log("Inside the homepage callback function")
+  console.log(req.sessionID)
+  res.send(`you just hit the home page yay!`)
 })
 
 // tell the server what port to listen on
@@ -14,5 +31,7 @@ app.listen(3000, ()=> {
 })
 
 
-
-
+  // in the session configuration below, 
+  // leaving the ‘secret’ as ‘keyboard cat’, 
+  // but in production this would be replaced with a randomly 
+  // generated string that’s pulled from an environment variable
